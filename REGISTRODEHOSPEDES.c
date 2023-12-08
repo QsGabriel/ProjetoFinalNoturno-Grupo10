6f878adc916b1994a5071333bcc49b68f39b7415
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2023-12-07 23:29:10 -0300

    Segundo Commit - Versão 1.2: listar e buscar hospedes (Daniel Vcd c:\Users\gabri\OneDrive\Documents\GitHub\ProjetoFinalNoturno-Grupo10\ProjetoFinalNoturno-Grupo10\output

--- REGISTRODEHOSPEDES.c
+++ REGISTRODEHOSPEDES.c
@@ -1,30 +1,88 @@
 #include <stdio.h>
+#include <string.h>
+
 #define NUM_QUARTOS 10
+#define TAM_NOME 50
+
 int i = 0;
- 
+typedef struct {
+    char nome[TAM_NOME];
+    int quarto;
+} Hospede;
+
 // Função para inserir um novo hóspede em um quarto vazio
-void inserirHospede(char quartos[], int numeroQuarto) {
-    if (quartos[numeroQuarto - 1] == 'V') {
-        quartos[numeroQuarto - 1] = 'O'; // 'O' representa ocupado
+void inserirHospede(Hospede hospedes[], int numeroQuarto) {
+    if (hospedes[numeroQuarto - 1].quarto == 0) {
+        hospedes[numeroQuarto - 1].quarto = numeroQuarto;
+        printf("Digite o nome do hospede: ");
+        scanf("%s", hospedes[numeroQuarto - 1].nome);
         printf("Hóspede inserido no quarto %d com sucesso.\n", numeroQuarto);
     } else {
         printf("O quarto %d está ocupado. Escolha outro quarto.\n", numeroQuarto);
     }
 }
+
+void listarHospedes(Hospede hospedes[]) {
+    printf("Lista de Hospedes:\n");
+    for (int i = 0; i < NUM_QUARTOS; i++) {
+        if (hospedes[i].quarto != 0) {
+            printf("Quarto %d: %s\n", hospedes[i].quarto, hospedes[i].nome);
+        }
+    }
+}
+
+void buscarHospede(Hospede hospedes[]) {
+    char nome[TAM_NOME];
+    printf("Digite o nome do hospede: ");
+    scanf("%s", nome);
+
+    for (int i = 0; i < NUM_QUARTOS; i++) {
+        if (hospedes[i].quarto != 0 && strcmp(hospedes[i].nome, nome) == 0) {
+            printf("Hospede encontrado no quarto %d.\n", hospedes[i].quarto);
+            return;
+        }
+    }
+
+    printf("Hospede nao encontrado.\n");
+}
 // Função principal
 int main() {
-    char estadoQuartos[NUM_QUARTOS]; // Array para armazenar o estado dos quartos ('V' para vazio, 'O' para ocupado)
-    int numeroHospede;
- 
+    Hospede hospedes[NUM_QUARTOS];
+    int opcao, numeroQuarto;
+
     // Inicializar todos os quartos como vazios
-    for (i; i < NUM_QUARTOS; i++) {
-        estadoQuartos[i] = 'V';
+    for (i = 0; i < NUM_QUARTOS; i++) {
+        hospedes[i].quarto = 0;
     }
- 
-    // Solicitar número do hóspede ao usuário
-    printf("Digite o numero do hospede: ");
-    scanf("%d", &numeroHospede);
- 
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Inserir um novo hospede em um quarto vazio\n");
+        printf("2. Listar hospedes\n");
+        printf("3. Buscar hospede\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Digite o numero do quarto: ");
+                scanf("%d", &numeroQuarto);
+                inserirHospede(hospedes, numeroQuarto);
+                break;
+            case 2:
+                listarHospedes(hospedes);
+                break;
+            case 3:
+                buscarHospede(hospedes);
+                break;
+            case 0:
+                printf("Saindo do programa.\n");
+                break;
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    } while (opcao != 0);
 
     return 0;
 }
